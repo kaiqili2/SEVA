30f22ea3deaa301892cfce64907c2a3d17cec925
kaiqili2 <kaiqili2-c@my.cityu.edu.hk>
2026-08-04 21:22:08 +0800

    Add files via upload

---
+++ src/model_sevafast.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import math
+from typing import Optional
+from typing import Sequence, Tuple, List, Union
+import torch.nn.functional as F
+import itertools
+import argparse
+import esm
+from torch import Tensor
+from torchvision.transforms import Compose, Resize, ToTensor
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+
+
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, in_channels: int = 768, n_classes: int = 3, dropout: float = 0.2):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        self.dropout = dropout
+
+
+        self.classify = nn.Sequential(
+            nn.LayerNorm(in_channels),
+            nn.Linear(in_channels, 512),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(512, 256),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(256, n_classes)
+        )
+
+
+    def forward(self, x):
+
+        x = self.classify(x)
+
+        return x
+
+
+
+class SEVA(nn.Module):
+    def __init__(self,
+                 in_channels: int = 934,
+                 n_classes: int = 3,
+                 dropout: float = 0.2
+
+                 ):
+        super().__init__()
+
+
+
+        self.classification_layer = ClassificationHead(in_channels, n_classes, dropout)
+
+    def forward(self, x, feature=None):
+        # x = [batch size, seq_len, emb_dim] dist_map = [batch size, dim, seq_len, seq_len] mask = [batch size, seq_len]
+
+        x = self.classification_layer(x)
+
+
+        return x
+
+
+if __name__ == '__main__':
+    x = torch.randn((1,934))
+
+
+    model = SEVA()
+    y = model(x)
+    print(model)
+    print(y)
---
+++ src/seva_fast_predict.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+import numpy as np
+import torch
+import os, datetime, argparse
+from features.statistical_feature import get_statistical_feature
+import time
+
+import matplotlib.pyplot as plt
+
+
+def test(sequence, model_file):
+
+
+    other_feature = get_statistical_feature(sequence)
+    other_feature = torch.tensor(other_feature, dtype=torch.float)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = torch.load(model_file)
+    model.eval()
+    with torch.no_grad():
+
+        other_feature = other_feature.to(device).unsqueeze(0)
+
+        pred = model(other_feature)
+        pred = pred.argmax(dim=1)
+
+        pred = pred.cpu().numpy().flatten()
+
+        if pred == 0:
+            print("This sequence is predicted to be an ARG")
+        elif pred == 1:
+            print("This sequence is predicted to be a VF")
+        elif pred == 2:
+            print("This sequence is predicted to be neither an ARG nor a VF")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sequence", type=str, help="protein sequence")
+    parser.add_argument("--model_file", type=str, help="model file")
+    args = parser.parse_args()
+
+    # test(sequence=args.sequence,
+    #      model_file=args.model_file, )
+
+    test('MFEIHPVKKVSVVIPVYNEQESLPELIRRTTAACESLGKEYEILLIDDGSSDNSAHMLVEASQAEGSHIVSILLNRNYGQHSAIMAGFSHVTGDLIITLDADLQNPPEEIPRLVAKADEGYDVVGTVRQNRQDSWFRKTASKMINRLIQRTTGKAMGDYGCMLRAYRRHIVDAMLHCHERSTFIPILANIFARRAIEIPVHHAEREFGESKYSFMHLINLMYDLVTCLTTTPLRMLSLLGSIIAIGGFSIAVLLVILRLTFGPQWAAEGVFMLFAVLFTFIGAQFIGMGLLGEYIGRIYTDVRARPRYFVQQVIRPSSKENE',
+         "SEVA_fast.pt"
+         )
+
+    # python seva_fast_predict.py --sequence  'MFEIHPVKKVSVVIPVYNEQESLPELIRRTTAACESLGKEYEILLIDDGSSDNSAHMLVEASQAEGSHIVSILLNRNYGQHSAIMAGFSHVTGDLIITLDADLQNPPEEIPRLVAKADEGYDVVGTVRQNRQDSWFRKTASKMINRLIQRTTGKAMGDYGCMLRAYRRHIVDAMLHCHERSTFIPILANIFARRAIEIPVHHAEREFGESKYSFMHLINLMYDLVTCLTTTPLRMLSLLGSIIAIGGFSIAVLLVILRLTFGPQWAAEGVFMLFAVLFTFIGAQFIGMGLLGEYIGRIYTDVRARPRYFVQQVIRPSSKENE' --model_file SEVA_fast.pt
